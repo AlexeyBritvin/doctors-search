f730e272f9607063218e8177a4cdd9ccc29883b5
Aleksey <krugombeda@gmail.com>
2020-10-25 22:26:29 +0300

    create input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Icon from '../Icon/Icon';
+import { IconTypes } from '../Icon/icon-path';
+import styles from './Input.module.css';
+
+export interface InputProps {
+  placeholder?: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  icon?: IconTypes;
+  iconSize?: string;
+  iconViewbox?: string;
+}
+
+const Input: React.FunctionComponent<InputProps> = ({
+  placeholder,
+  value,
+  onChange,
+  icon,
+  iconSize,
+  iconViewbox
+}) => {
+  return (
+    <div className={styles.inputBox}>
+      <input type="text" className={styles.input} placeholder={placeholder} value={value} onChange={onChange} />
+      { icon && <Icon className={styles.icon} icon={icon} viewBox={iconViewbox} size={iconSize}></Icon> }
+    </div>
+  );
+}
+
+export default Input;
