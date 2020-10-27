940cf503b752ccd38ce0637b6f37cc0dbc9b779b
Aleksey <krugombeda@gmail.com>
2020-10-28 00:43:02 +0300

    create filter result item component

---
+++ src/components/FilterResultCard/FilterResultCard.tsx
@@ -0,0 +1,59 @@
+import React, { FunctionComponent } from 'react';
+import Avatar from '../Avatar/Avatar';
+import Icon from '../Icon/Icon';
+import styles from './FilterResultCard.module.css';
+
+export interface FilterResultCardProps {
+  avatar?: string;
+  name: string;
+  address: string;
+  speciality: string;
+  experience: number;
+  reviewsCount: number;
+  telehealth: boolean;
+  price: number;
+}
+
+const FilterResultCard: FunctionComponent<FilterResultCardProps> = ({
+  avatar,
+  name,
+  speciality,
+  experience,
+  reviewsCount,
+  address,
+  telehealth,
+  price
+}) => {
+  return (
+    <div className={styles.card}>
+      <Avatar source={avatar}></Avatar>
+      <div className={styles.cardTextInfo}>
+        <div className={styles.cardTitle}>
+          <p>{name}</p>
+        </div>
+        <div className={styles.cardTextRow}>
+          <span className={styles.cardTextItem}>{speciality}</span>
+          <span className={styles.cardTextItem}>{experience} Years Experience</span>
+          <span className={`${styles.cardTextItem} ${styles.highlighed}`}>{reviewsCount} Reviews</span>
+        </div>
+        <div className={styles.cardAdditionalText}>
+          {
+            telehealth && <span className={styles.cardTextItem}>Video visit</span>
+          }
+          <span className={styles.cardTextItem}>{address}</span>
+        </div>
+      </div>
+
+      <div className={styles.cardActions}>
+        <div className={styles.cardPrice}>
+          <span className={styles.cardPriceTitle}>avg. price</span>
+          <span className={styles.cardPriceValue}>${price}</span>
+        </div>
+
+        <Icon className={styles.icon} icon="heart" viewBox="0 0 22 17" size="22"></Icon>
+      </div>
+    </div>
+  );
+}
+
+export default FilterResultCard;
