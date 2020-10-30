bfa837b670f488a29b54236b1494d18b3cac3ce6
Aleksey <krugombeda@gmail.com>
2020-10-30 15:14:22 +0300

    WIP: demo search page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,62 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
+import { ReactComponent as InfoIcon } from '../../icons/info.svg';
+import Checkbox from '../Checkbox/Checkbox';
+import CheckboxWithCount from '../CheckboxWithCount/CheckboxWithCount';
+import Dropdown from '../Dropdown/Dropdown';
 import FilterResultCard from '../FilterResultCard/FilterResultCard';
-import Icon from '../Icon/Icon';
+import SearchInput from '../SearchInput/SearchInput';
+import Switcher from '../Switcher/Switcher';
 import styles from './App.module.css';
 
 function App() {
 
+  const [inputValue, setInputValue] = useState('')
+
+  const [checked, setChecked] = useState(false)
+
+  const [value, onSwitcherChange] = useState(false)
+
+  const handleChange = ({target}: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(target.value)
+  }
+
+  const handleSwitcher = ({target}: ChangeEvent<HTMLInputElement>) => {
+    onSwitcherChange(target.checked)
+  }
+
+  const onCheckedChange = ({target}: ChangeEvent<HTMLInputElement>) => setChecked(target.checked)
+
   return (
     <div className={styles.app}>
       <div className={styles.container}>
+        <Dropdown label="Button">
+          <SearchInput
+            value={inputValue}
+            placeholder="Filter by speciality"
+            onChange={handleChange}
+          ></SearchInput>
+
+          <div>
+            <div>
+              <Switcher value={value} onChange={handleSwitcher}></Switcher>
+            </div>
+
+            <Checkbox
+              checked={checked}
+              onChange={onCheckedChange}
+            >Plastic Surgeon</Checkbox>
+
+            <CheckboxWithCount
+              checked={checked}
+              onChange={onCheckedChange}
+              count={4}
+            >Plastic Surgeon</CheckboxWithCount>
+          </div>
+        </Dropdown>
+
         <h1 className={styles.header}>Root Canal doctors in New York, NY</h1>
         <div className={styles.info}>
-          <Icon icon="info" size="20" viewBox="0 0 20 20" className={styles.infoIcon}></Icon>
+          <InfoIcon className={styles.infoIcon}></InfoIcon>
           <div className={styles.infoText}>
             <p>The average price of a procedure in New York is $300</p>
           </div>
