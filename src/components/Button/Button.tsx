f9bb93b18e2e7f7e43fc9be538a5f7213e82cfa8
Aleksey <krugombeda@gmail.com>
2020-10-31 14:45:25 +0300

    add onClick prop

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,16 @@
 import * as React from 'react';
+import { SyntheticEvent } from 'react';
 import styles from './Button.module.css';
 
 export interface ButtonProps {
   viewType: 'link' | 'primary';
+  onClick?: (event: SyntheticEvent) => void;
 }
 
-const Button: React.FunctionComponent<ButtonProps> = (props) => {
-  const {viewType} = props;
+const Button: React.FunctionComponent<ButtonProps> = ({viewType, onClick, children}) => {
   return (
-    <button type="button" className={styles[viewType]}>
-      {props.children}
+    <button type="button" className={styles[viewType]} onClick={onClick}>
+      {children}
     </button>
   );
 }
