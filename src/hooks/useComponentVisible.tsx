86a5572dd3949a9312a028df5e81a7e9fdc0f5ba
Aleksey <krugombeda@gmail.com>
2020-11-18 16:47:22 +0300

    replace methods inside hook

--- src/hooks/useComponentVisible.tsx
+++ src/hooks/useComponentVisible.tsx
@@ -1,34 +1,31 @@
-import { useEffect, useRef, useState } from 'react';
-
+import { useEffect, useRef, useState } from 'react'
 
 function useComponentVisible(initialIsVisible: boolean, onClose?: () => void) {
-  const [isComponentVisible, setIsComponentVisible] = useState(
-    initialIsVisible
-  );
+  const [isComponentVisible, setIsComponentVisible] = useState(initialIsVisible)
   const ref = useRef<HTMLDivElement>(null)
 
-  const handleHideDropdown = (event: KeyboardEvent) => {
-    if (event.key === 'Escape') {
-      setIsComponentVisible(false)
-      onClose && onClose()
-    }
-  };
-
-  const handleClickOutside = (event: MouseEvent) => {
-    if (ref.current && !ref.current.contains(event.target as HTMLDivElement)) {
-      setIsComponentVisible(false)
-      onClose && onClose()
-    }
-  };
-
   useEffect(() => {
+    const handleHideDropdown = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        setIsComponentVisible(false)
+        onClose && onClose()
+      }
+    }
+
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as HTMLDivElement)) {
+        setIsComponentVisible(false)
+        onClose && onClose()
+      }
+    }
+
     document.addEventListener('keydown', handleHideDropdown, true)
     document.addEventListener('click', handleClickOutside, true)
     return () => {
       document.removeEventListener('keydown', handleHideDropdown, true)
       document.removeEventListener('click', handleClickOutside, true)
-    };
-  }, [isComponentVisible]);
+    }
+  }, [isComponentVisible])
 
   return { ref, isComponentVisible, setIsComponentVisible }
 }
