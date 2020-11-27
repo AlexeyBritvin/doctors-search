3696492e83637224a3fc7e4bc53684ef5ba2ef44
Aleksey <krugombeda@gmail.com>
2020-11-27 17:02:27 +0300

    add insurance switcher

--- src/components/Filters/InsuranceFIlter/InsuranceFilter.tsx
+++ src/components/Filters/InsuranceFIlter/InsuranceFilter.tsx
@@ -2,9 +2,13 @@ import React, { ChangeEvent, useEffect, useState } from 'react'
 import useGenerateCheckboxes from '../../../hooks/useGenerateCheckboxes'
 import { CheckboxView } from '../../../models/checkbox-view.model'
 import CheckboxWithCount from '../../CheckboxWithCount/CheckboxWithCount'
+import Divider from '../../Divider/Divider'
 import Dropdown from '../../Dropdown/Dropdown'
 import DropdownMenuItem from '../../Dropdown/DropdownMenuItem/DropdownMenuItem'
 import SearchInput from '../../SearchInput/SearchInput'
+import Switcher from '../../Switcher/Switcher'
+
+import styles from './InsuranceFilter.module.css'
 
 export interface InsuranceFilterProps {
   insurance: string[]
@@ -14,6 +18,7 @@ export interface InsuranceFilterProps {
 
 const InsuranceFilter: React.FC<InsuranceFilterProps> = ({ insurance, selected, onChange }) => {
   const [search, setSearch] = useState('')
+  const [otherThenInsuranse, setOtherThenInsuranse] = useState(false)
   const checkboxes = useGenerateCheckboxes(insurance, search)
   const [innerSelected, setSelected] = useState<string[]>([])
 
@@ -48,8 +53,23 @@ const InsuranceFilter: React.FC<InsuranceFilterProps> = ({ insurance, selected, 
     setSearch('')
   }
 
+  const handleSwitcherChange = () => {
+    setOtherThenInsuranse(!otherThenInsuranse)
+  }
+
   return (
     <Dropdown label="Insurance" size="big" footer={true} onApply={handleApply} onReset={handleReset} onClose={onClose}>
+      <DropdownMenuItem>
+        <div className={styles.switcherWrap}>
+          <span>Provides other than insurance payment options</span>
+          <Switcher value={otherThenInsuranse} onChange={handleSwitcherChange}></Switcher>
+        </div>
+      </DropdownMenuItem>
+
+      <DropdownMenuItem>
+        <Divider />
+      </DropdownMenuItem>
+
       <DropdownMenuItem>
         <SearchInput
           value={search}
